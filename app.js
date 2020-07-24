933af67b0d533ca197a9ddd65080c42a3ee6ceb9
georgina puig <georginaxcodes@gmail.com>
2020-07-24 01:44:43 -0500

    add secret route and template

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
+app.get('/secret', function(req, res) {
+  res.render('secret');
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
   console.log('server has started');
 });
