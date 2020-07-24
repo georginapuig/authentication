bcf50c05664f52b9f933edeb9bb5a8c7ecb24f0c
georgina puig <georginaxcodes@gmail.com>
2020-07-24 01:31:55 -0500

    install express-session

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // npm install express mongoose --save
 // npm install passport passport-local --save
 // npm install passport-local-mongoose --save
+// npm install body-parser --save
+// npm install express-session --save 
