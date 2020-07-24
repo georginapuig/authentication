c23f26ee6703231261e93f712ae8474830d6b937
georgina puig <georginaxcodes@gmail.com>
2020-07-24 01:41:22 -0500

    add root route and template

--- app.js
+++ app.js
@@ -4,3 +4,16 @@
 // npm install body-parser --save
 // npm install express-session --save
 // npm install ejs --save
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+  console.log('server has started');
+});
