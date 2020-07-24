bdb2974eddccc3712fc94a2d717294736bfbe31c
georgina puig <georginaxcodes@gmail.com>
2020-07-24 01:35:59 -0500

    install ejs

--- app.js
+++ app.js
@@ -2,4 +2,5 @@
 // npm install passport passport-local --save
 // npm install passport-local-mongoose --save
 // npm install body-parser --save
-// npm install express-session --save 
+// npm install express-session --save
+// npm install ejs --save
